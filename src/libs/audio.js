b8f1fd73e5e33f8a1c89a0a5b377a8470eccd530
maelon <maelon.j@gmail.com>
2016-12-27 19:14:20 +0800

    backup

---
+++ src/libs/audio.js
@@ -0,0 +1,22 @@
+'use strict';
+
+class Audio {
+    construct(conf) {
+        this._config = conf;
+    }
+
+    /**
+    * 
+    */
+    play(t = 0) {
+    }
+
+    stop() {
+    }
+
+    pause() {
+    }
+
+    reset() {
+    }
+}
